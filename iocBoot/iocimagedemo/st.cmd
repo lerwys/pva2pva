28f83e3e0ef9c5ca98a7a12e8aebc9ee4ca88715
Lucas Russo <lerwys@gmail.com>
2021-08-17 09:33:08 -0300

    ndaroi: add demo ROI

--- iocBoot/iocimagedemo/st.cmd
+++ iocBoot/iocimagedemo/st.cmd
@@ -2,6 +2,7 @@
 
 dbLoadRecords("image.db","N=TST:image1")
 dbLoadRecords("ndain.db","N=TST:image2")
+dbLoadRecords("ndaroi.db","N=TST:image3")
 dbLoadRecords("table.db","N=TST:table1")
 
 iocInit()
